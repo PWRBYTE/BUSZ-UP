f2073c302d5350dd9251ca1dc52c98d073705139
arijitghosh120403 <arijitghosh120403@gmail.com>
2023-02-05 05:18:35 +0530

    Final Staged Commits

---
+++ Employee-Reviews/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const URL = process.env.URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
